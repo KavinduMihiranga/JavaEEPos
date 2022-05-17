40cd2cf37f1bfdffb1a8398ffcb760c03326293d
KavinduMihiranga <mihirangak1999@gmail.com>
2022-05-17 15:56:14 +0530

    add save order

--- JavaEE_PosSystem/src/servlet/OrderServlet.java
+++ JavaEE_PosSystem/src/servlet/OrderServlet.java
@@ -1,6 +1,8 @@
 package servlet;
 
 import javax.annotation.Resource;
+import javax.json.Json;
+import javax.json.JsonObjectBuilder;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,6 +10,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 @WebServlet(urlPatterns = "/order")
 public class OrderServlet extends HttpServlet {
@@ -16,6 +22,76 @@ public class OrderServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+
+        try {
+            String option = req.getParameter("option");
+            resp.setContentType("application/json");
+            Connection connection = ds.getConnection();
+            PrintWriter writer = resp.getWriter();
+
+            resp.addHeader("Access-Control-Allow-Origin", "*");
+
+            switch (option){
+                case "SEARCH":
+                    //
+                    break;
+                case "GETALL":
+                    connection.prepareStatement(" ");
+                    break;
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        
+        String orderId = req.getParameter("orderId");
+        String orderDate = req.getParameter("orderDate");
+        String orderCustomerName = req.getParameter("orderCustomerName");
+        String orderCustomerId = req.getParameter("orderCustomerId");
+        String orderCustomerAddress = req.getParameter("orderCustomerAddress");
+
+        String itemCode = req.getParameter("orderItemCode");
+        String orderItemName = req.getParameter("orderItemName");
+        String orderQty = req.getParameter("orderQty");
+        String orderQtyOnHand = req.getParameter("orderQtyOnHand");
+
+
+        resp.addHeader("Access-Control-Allow-Origin", "*");
+
+        PrintWriter writer = resp.getWriter();
+        resp.setContentType("application/json");
+
+        try {
+            Connection connection = ds.getConnection();
+            PreparedStatement pstm = connection.prepareStatement("Insert into `Order` values(?,?,?,?)");
+            pstm.setObject(1,orderId);
+            pstm.setObject(2,orderCustomerId);
+            pstm.setObject(3,orderDate);
+            pstm.setObject(4,orderQty);
+
+            if (pstm.executeUpdate()>0){
+                JsonObjectBuilder response = Json.createObjectBuilder();
+                resp.setStatus(HttpServletResponse.SC_CREATED);//201
+                response.add("status", 200);
+                response.add("message", "Successfully Added");
+                response.add("data", "");
+                writer.print(response.build());
+            }
+            connection.close();
+
+        } catch (SQLException throwables) {
+            JsonObjectBuilder response = Json.createObjectBuilder();
+            response.add("status", 400);
+            response.add("message", "Error");
+            response.add("data", throwables.getLocalizedMessage());
+            writer.print(response.build());
+            resp.setStatus(HttpServletResponse.SC_OK); //200
+            throwables.printStackTrace();
+        }
     }
 }
